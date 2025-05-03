c3b63c39298a41a3489827627d8f09ac1ffec473
mergutm <merg@gs.utm.mx>
2025-05-03 14:44:12 -0600

    primera version

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:recipes_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recetario',
+      home: Recetario(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Recetario extends StatelessWidget {
+  const Recetario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: colors.primary,
+          title: Text("Recetario", style: TextStyle(color: Colors.white)),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: colors.tertiary,
+            tabs: <Widget>[Tab(icon: Icon(Icons.home), text: 'Inicio')],
+          ),
+        ),
+        body: //HomeScreen(),
+            TabBarView(children: [HomeScreen()]),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          _RecipesCard(context),
+          _RecipesCard(context),
+          _RecipesCard(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _RecipesCard(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: colors.tertiary,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.network(
+                    'https://myplate-prod.azureedge.us/sites/default/files/styles/recipe_525_x_350_/public/2022-01/Noodles_1.jpg?itok=D8SbUIWg',
+                    //'https://www.excelsior.com.mx/800x600/filters:format(webp):quality(75)/media/pictures/2025/05/03/3301163.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                // child: ClipRRect(
+                //   borderRadius: BorderRadius.circular(10),
+                //   child: Container(),
+                //   //child: Text("rpu"),
+                // ),
+              ),
+              SizedBox(width: 20, height: 10),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Frijoles charros",
+                    style: TextStyle(fontSize: 16, fontFamily: 'QuickSand'),
+                  ),
+                  Container(width: 100, height: 3, color: colors.primary),
+                  SizedBox(height: 5),
+                  Text(
+                    "Caroline",
+                    style: TextStyle(fontSize: 12, fontFamily: 'QuickSand'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
